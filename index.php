1910186373a51f0f0746ba18ba44b8df67c39c93
LabKitty <labkittyx@gmail.com>
2014-10-27 10:48:22 +0200

    Add File

--- index.php
+++ index.php
@@ -1,34 +1,70 @@
 <?php require("validation.php"); ?>
+
 <!DOCTYPE HTML>
+
 <html>
-<head>
-	<meta charset="UTF-8">
-	<title>2. periood 6. praktikum</title>
-	<style>
-		.form_field{
-			margin-top:10px;
-		}
+   <head>
+		<title>2. periood -6 ja 7-praktikum</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<style>
 		.form_message{
-			font-weight:bold;
+			font-weight: bold;
 		}
-	</style>
-</head>
-<body>
-	<form action="index.php" method="post">
-		<div class="form_field">
-			<div class="form_message">
-				<?php if ($isSubmitted){
-					echo $usernameMessage;
-				}
-				?>
-			</div>
-		<span>Kasutajanimi:</span>
-			<input type="text" name="username">
-		</div>
-		<div class="form_field">
-			<input type="submit" name="submit" value="Saada">
-		</div>
-	</form>
-	<pre><?php print_r($_POST); ?></pre>
+      .form_error{
+         color: red;
+      }
+   .form_notice{
+         color: green;
+      }
+		</style>
+  
+   </head>
+  
+ <body>
+   	<pre><?php print_r($_POST); ?></pre>	
+      
+
+<form action="index.php" method="post">
+   	
+   <div class= "form_message">
+   	<?php if ($isSubmitted) {echo $usernameMessage;}?>
+   		
+         <label for="username">Kasutajanimi:</label>
+   	
+      <input id="username" type="text" name="username">
+   </div>
+   <br>
+   <div class= "form_message">
+      <?php  if ($isSubmitted) {echo $ageMessage;}?>
+         
+         <label for="age">Vanus:</label>
+      
+      <input id="age" type="text" name="age">
+   </div>
+   <br>
+     <div class= "form_message">
+      <?php  if ($isSubmitted) {echo $foodMessage;}?>
+         
+         <label for="name">Food:</label>
+      
+      <input id="food" type="text" name="food">
+   </div>
+   
+<br>
+      <div class= "form_message">
+      <?php  if ($isSubmitted) {echo $emailMessage;}?>
+         
+         <label for="email">E-mail:</label>
+      
+      <input id="email" type="text" name="email">
+   </div>
+
+   <div>
+   	
+      <input type="submit" value="Saada" name="submit">
+  
+   </div>
+
+</form>
 </body>
 </html>
